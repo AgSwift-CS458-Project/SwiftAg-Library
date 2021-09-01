dbe52b4c390e6b1b94c4dcc8af036c91125d1894
Jordan Abbott <abbottjord94@gmail.com>
2021-09-01 08:52:12 -0700

    Fixed issue with point-in-triangle function

--- SwiftAg/tests.cpp
+++ SwiftAg/tests.cpp
@@ -18,8 +18,11 @@ int main(int argc, char* argv[]) {
 	Point p1(0, 0, 1);
 	Point p2(0, 5, 1);
 	Point p3(5, 0, 1);
+	Point p4(5, 5, 1);
+	Point tp(2, 2, 0);
 	Edge e1(&p1, &p2);
 	Triangle t1(&p1, &p2, &p3);
+	Triangle t2(&p2, &p3, &p4);
 	Graph g1;
 	Graph g2;
 	cout << "Point, Edge, & Triangle Tests:" << endl;
@@ -27,21 +30,25 @@ int main(int argc, char* argv[]) {
 	cout << "Edge type size: " << sizeof(e1) << endl;
 	cout << "Triangle type size: " << sizeof(t1) << endl;
 	cout << "Point 1 (0,0,1)" << endl;
-	cout << "Point 2 (0,5,1)" << endl;
-	cout << "Point 3 (5,0,1)" << endl;
+	cout << "Point 2 (0,1,1)" << endl;
+	cout << "Point 3 (1,0,1)" << endl;
 	cout << "Edge 1 (Point 1, Point 2)" << endl;
 	cout << "Triangle 1 (Point 1, Point 2, Point 3)" << endl;
 	cout << "Point Distance: " << p1.distance(&p2) << endl;
 	cout << "Edge Length: " << e1.length() << endl;
 	cout << "Point Distance and Edge Length should be equal: " << (p1.distance(&p2) == e1.length()) << endl;
-	cout << "Point (2,2,0) inside Triangle 1: " << t1.pointInTriangle(new Point(2, 2, 0)) << endl;
-	cout << "Height at Point (2,2,0) inside Triangle 1: " << t1.getHeightAtPoint(new Point(2, 2, 0)) << endl;
-	cout << "Point (-2, -2, 0) inside Triangle 1: " << t1.pointInTriangle(new Point(-2, -2, 0)) << endl;
+	cout << "Point (0.5,0.5,0) inside Triangle 1: " << t1.pointInTriangle(&tp) << endl;
+	cout << "Height at Point (2,2,0) inside Triangle 1: " << t1.getHeightAtPoint(&tp) << endl;
+	cout << "Point (-0.5, -0.5, 0) inside Triangle 1: " << t1.pointInTriangle(new Point(-2, -2, 0)) << endl;
+	cout << "Point 4 (1,1,1)" << endl;
+	cout << "Triangle 2 (Point 2, Point 3, Point 4)" << endl;
+	cout << "Point (0.5,0.5,0) inside Triangle 2: " << t2.pointInTriangle(&tp) << endl;
+	cout << "Point (-0.5, -0.5, 0) inside Triangle 2: " << t2.pointInTriangle(new Point(2, -2, 0)) << endl;
 	cout << endl;
 	cout << "Graph Tests: " << endl;
 	cout << "Graph 1: 25 random points" << endl;
 	for (int i = 0; i < 25; i++) {
-		Point* new_point = new Point((rand() % 1000), (rand() % 700), (double)(rand() % 9) + 3);
+		Point* new_point = new Point((rand() % 1000), (rand() % 700), 3);
 		g1.addPoint(new_point);
 	}
 	cout << "Number of points added: " << g1.pointCount() << endl;
@@ -51,7 +58,6 @@ int main(int argc, char* argv[]) {
 	}
 	cout << "Generating Delaunay Triangulation..." << endl;
 	g1.generateDelaunayTriangulation();
-	//g1.bowyerWatsonTriangulation();
 	ofstream ex_outfile;
 	ex_outfile.open("output-existing.json");
 	ex_outfile << "[";
@@ -67,12 +73,6 @@ int main(int argc, char* argv[]) {
 
 	cout << "Creating second graph: " << endl;
 
-	/*for (int x = 0; x < 5; x++) {
-		for (int y = 0; y < 5; y++) {
-			Point* p = new Point(200 + (x * 80), 200 + (y * 80), 3);
-			g2.addPoint(p);
-		}
-	}*/
 	for (int i = 0; i < 25; i++) {
 		Point* new_point = new Point(300+(rand() % 300), 300+(rand() % 300), 3);
 		g2.addPoint(new_point);
@@ -94,10 +94,12 @@ int main(int argc, char* argv[]) {
 	pr_outfile.close();
 
 	MeshComparator mc(&g1, &g2);
-	mc.calculateCutFill(10);
+	mc.calculateCutFill(5);
 	cout << "Cut Amount: " << mc.getCutAmount() << endl;
 	cout << "Fill Amount: " << mc.getFillAmount() << endl;
-	cout << "Cut - Fill difference: " << mc.getCutAmount() - mc.getFillAmount() << endl;
+	cout << "Cut - Fill difference: " << mc.getCutAmount() + mc.getFillAmount() << endl;
+
+	cout << "Cut-fill amounts should be zero (or near zero)" << endl;
 
 	return 0;
 }
